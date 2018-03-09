71cd7b3a2f9eedb1af0bcf5d0eed041f437d68ec
liliwaukee <lilimmvisual@gmail.com>
2018-03-09 16:46:24 +0000

    agregando estilos

--- js/data.js
+++ js/data.js
@@ -1,42 +1,42 @@
 var data = {
   products: [
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/switch.png",
       title: "Nintendo Switch",
       description: "Just another description",
       id: 1,
       price: 3900
     },
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/ps4.png",
       title: "Play Station 4",
       description: "Just another description",
       id: 2,
       price: 6900
     },
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/ps3.png",
       title: "Play Station 3",
       description: "Yes",
       id: 3,
       price: 6500
     },
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/ps2.png",
       title: "Play Station 2",
       description: "Yes",
       id: 4,
       price: 1000
     },
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/ps1.png",
       title: "Play Station 1",
       description: "Yes",
       id: 5,
       price: 9500
     },
     {
-      imageUrl: "http://dummyimage.com/100x100",
+      imageUrl: "assets/images/nintendo.png",
       title: "Nintendo 64",
       description: "Yes",
       id: 6,
