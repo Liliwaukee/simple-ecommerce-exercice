78c2f85bdaa12eb7961b63a4b5793c38d7efdbce
liliwaukee <lilimmvisual@gmail.com>
2018-03-09 16:12:25 +0000

    agregando precio total de productos

--- js/checkout.js
+++ js/checkout.js
@@ -30,11 +30,57 @@ function calculateTotal(productsInCart) {
 
   });
 
-  document.write("TOTAL: " + totalPrice);
 
   let showTotalPrice = `
-  <td>${totalPrice}</td>
+  <td><td>
+  <td><h3>${totalPrice}</h3></td>
   `;
   const totalPriceContainer = document.querySelector("#total-price");
   totalPriceContainer.innerHTML = showTotalPrice;
+
+  paypalPay(totalPrice)
+}
+
+
+function paypalPay(totalPrice){
+    paypal.Button.render({
+
+      env: 'sandbox', // sandbox | production
+
+      // PayPal Client IDs - replace with your own
+      // Create a PayPal app: https://developer.paypal.com/developer/applications/create
+      client: {
+          sandbox:    'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R',
+          production: 'AZwtebwVV7oNYII1ZGc4tZF74uhotXDvle87uMGMeSvwvVvZ4oJTQ6JtR611__YSOvXFDyUVaa0aT70V'
+      },
+
+      // Show the buyer a 'Pay Now' button in the checkout flow
+      commit: true,
+
+      // payment() is called when the button is clicked
+      payment: function(data, actions) {
+
+          // Make a call to the REST api to create the payment
+          return actions.payment.create({
+
+              payment: {
+                  transactions: [
+                      {
+                          amount: { total: `${totalPrice}`, currency: 'MXN' }
+                      }
+                  ]
+              }
+          });
+      },
+
+      // onAuthorize() is called when the buyer approves the payment
+      onAuthorize: function(data, actions) {
+
+          // Make a call to the REST api to execute the payment
+          return actions.payment.execute().then(function() {
+              window.alert('Payment Complete!');
+          });
+      }
+
+  }, '#paypal-button-container');
 }
