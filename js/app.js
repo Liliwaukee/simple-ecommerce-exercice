25ba3d723b382db7da59919c5947f1cba00d28f2
Liliwaukee <lilimmvisual@gmail.com>
2018-03-08 22:43:53 -0600

    agregando funcionalidad

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ function createProductHTML(product) {
     <img src='${product.imageUrl}' alt='${product.description}'/>
     <p>${product.description}</p>
     <button data-product-id=${product.id}
-      onclick="addToCart(${product.id})"
-      class='btn btn-primary'>
+      onclick="addToCart(this,${product.id})"
+      class="btn btn-primary">
         Agregar a carrito
       </button>
     <hr/>
@@ -27,16 +27,39 @@ function createProductHTML(product) {
 
 drawProducts(data);
 
-function addToCart() {
+let productsInCart = [];
+
+function addToCart(product, productId) {
   /* cuando agrego a carrito, tengo que:
   1) Incrementar en uno mi contador del menu
   2) Guardar mi producto en algun lugar
   3) Cambiar el boton de agregar a carrito
   por quitar del carrito
+
   */
+
+  let productItem = data.products[(productId-1)];
+  productsInCart.push(productItem);
+  localStorage.setItem("productsInCart", JSON.stringify(productsInCart));
+
+  const counter = document.querySelector('#counterItems');
+
+  changeButtonStatus(product);
+
+  if (product.classList.contains('btn-addCart') === false) {
+      increaseCounter(product, counter);
+
+    } else if (product.classList.contains('btn-addCart') === true) {
+      decreaseCounter(product, counter);
+
+    }
 }
 
-function removeFromCart() {
+
+
+
+
+function removeFromCart(product) {
   /* cuando agrego a carrito, tengo que:
   1) Decrementar en uno mi contador del menu
   2) Borrar mi producto de algun lugar
@@ -45,21 +68,42 @@ function removeFromCart() {
   */
 }
 
-function increaseCounter() {
+function increaseCounter(product, counter) {
   /* como accedemos al HTML del contador
   y como lo incrementamos*/
+  product.classList.toggle('btn-addCart')
+  let counterNumber = parseInt(document.querySelector('#counterItems').textContent);
+  counterNumber ++
+  counter.innerHTML = counterNumber;
+  productsCounter(counterNumber);
 }
 
-function decreaseCounter() {
+function decreaseCounter(product, counter) {
   /* como accedemos al HTML del contador
   y como lo incrementamos*/
+  product.classList.toggle('btn-addCart')
+  let counterNumber = parseInt(document.querySelector('#counterItems').textContent);
+  counterNumber --
+  counter.innerHTML = counterNumber;
+  productsCounter(counterNumber);
 }
 
 function changeButtonStatus(button) {
-  /* esta funcion deberia recibir un boton y
-  cambiar su estatus
-    Si el boton esta en agregar al carrito
-      cambia el texto a quitar del carrito
-    Y viceversa
-  */
+
+  if (button.innerText =="Agregar a carrito") {
+      button.innerText = "Quitar del carrito";
+      button.classList.remove("btn-primary");
+      button.classList.add("btn-secondary");
+
+  }
+   else {
+     button.innerText = "Agregar a carrito";
+     button.classList.add("btn-primary");
+     button.classList.remove("btn-secondary");
+
+  }
+}
+
+function productsCounter(counterNumber){
+  localStorage.setItem("itemsCounter", counterNumber);
 }
